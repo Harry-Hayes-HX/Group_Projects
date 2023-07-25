28198b8fa14f2ec4d8ba53568be66c44f7a71b8e
Harry-Hayes-HX <137900746+Harry-Hayes-HX@users.noreply.github.com>
2023-07-25 14:51:44 +0100

    design a single function exercise 1 - added more text sizes to test

--- reading_speed_calculator/tests/test_RSC.py
+++ reading_speed_calculator/tests/test_RSC.py
@@ -1,8 +1,17 @@
 from lib.reading_speed_calc import *
 
 def test_valid_filepath():
-    build = RSC("lib/text.txt")
-    assert build == "it will take 0.0 minutes"
+    build = RSC("lib/text1.txt")
+    assert build == "it will take 0.125 minutes"
+    build = RSC("lib/text2.txt")
+    assert build == "it will take 0.5 minutes"
+    build = RSC("lib/text3.txt")
+    assert build == "it will take 1.0 minutes"
+    build = RSC("lib/text4.txt")
+    assert build == "it will take 1.5 minutes"
+    build = RSC("lib/text5.txt")
+    assert build == "it will take 2.0 minutes"
+
 
 def test_invalid_filepath():
     build = RSC("guygu")
